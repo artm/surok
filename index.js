d97739b7c0f8df019a8193de4b4e8e1319d30292
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-03-19 12:14:57 +0100

    add a minimap and zoom in the main waveform

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 let WaveSurfer = require("wavesurfer");
 let wavesurfer = WaveSurfer.create({
   container: "#wave",
-  height: 30
+  height: 60,
+  barWidth: 2,
+  hideScrollbar: true
 });
 wavesurfer.load("./tmp/harry.mp3");
+
+require("wavesurfer/plugin/wavesurfer.minimap");
+let minimap;
+wavesurfer.on('ready', function () {
+  minimap = wavesurfer.initMinimap({
+    height: 30,
+    barHeight: 10,
+    barWidth: null
+  });
+  wavesurfer.zoom(25);
+});
 
 let $ = require("jquery");
 let play = $("#play-button");
