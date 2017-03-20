ed19cbe259a6278691182dc05fd29e969550a259
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-03-20 21:24:20 +0100

    unpair peaks
    wavesurfer returns two peaks for each waveform segement - max and min,
    we only care for abs max

--- segmentator.js
+++ segmentator.js
@@ -1,20 +1,29 @@
 export default class {
   findSegments(peaks, duration) {
+    peaks = this.unpairPeaks(peaks);
     let bits = this.threshold(this.normalize(peaks), 0.01);
     let peakLen = duration / peaks.length;
     let segments = this.segmentBits(bits, peakLen, 1, 0.4);
     return segments;
   }
 
+  unpairPeaks(peaks) {
+    let unpaired = []
+    for(let i = 0; i<peaks.length; i+=2) {
+      unpaired[i/2] = Math.max(Math.abs(peaks[i]), Math.abs(peaks[i+1]));
+    }
+    return unpaired;
+  }
+
   normalize(samples) {
     let max = 0;
     for(let i in samples) {
       let sample = samples[i];
-      max = Math.max(max, Math.abs(sample));
+      max = Math.max(max, sample);
     }
     let normalized = [];
     for(let i in samples) {
-      let sample = Math.abs(samples[i]);
+      let sample = samples[i];
       normalized.push(sample / max);
     }
     return normalized;
@@ -23,7 +32,7 @@ export default class {
   threshold(samples, threshold) {
     let bits = [];
     for(let i in samples) {
-      let sample = Math.abs(samples[i]);
+      let sample = samples[i];
       let bit = sample > threshold;
       bits.push(bit);
     }
