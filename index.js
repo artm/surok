95483d706ea63b31a6ff69fe22a1dba061db753e
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-03-30 07:18:58 +0200

    playtomaton creates wavesurfer

--- index.js
+++ index.js
@@ -1,40 +1,5 @@
-// no idea how to import these with ES6 syntax
-let WaveSurfer = require("wavesurfer");
-require("wavesurfer/plugin/wavesurfer.minimap");
-require("wavesurfer/plugin/wavesurfer.regions");
-
-import $ from "jquery";
-import Segmentator from "./segmentator";
 import Playtomaton from "./playtomaton";
 import React from "react";
 import ReactDOM from "react-dom";
 
-let wavesurfer = WaveSurfer.create({
-  container: "#wave",
-  height: 60,
-  barWidth: 2,
-  hideScrollbar: true,
-  interact: false
-});
-wavesurfer.load("./tmp/harry.mp3");
-
-let minimap;
-let segmentator;
-wavesurfer.on("ready", () => {
-  minimap = wavesurfer.initMinimap({
-    height: 30,
-    barHeight: 10,
-    barWidth: null,
-    interact: true
-  });
-  wavesurfer.zoom(25);
-  let peaks = wavesurfer.backend.getPeaks(100000, 0, 99999);
-  let segmentator = new Segmentator();
-  let segments = segmentator.findSegments(peaks, wavesurfer.getDuration());
-  for(let i in segments) {
-    wavesurfer.addRegion(segments[i]);
-  }
-});
-
-let playnode = $("#playtomaton").get(0);
-ReactDOM.render(<Playtomaton wavesurfer={wavesurfer}/>, playnode);
+ReactDOM.render(<Playtomaton src="./tmp/harry.mp3"/>, document.getElementById("playtomaton"));
--- playtomaton.js
+++ playtomaton.js
@@ -1,10 +1,14 @@
+// can these be imported with ES6 syntax?
+window.WaveSurfer = require("wavesurfer");
+require("wavesurfer/plugin/wavesurfer.minimap");
+require("wavesurfer/plugin/wavesurfer.regions");
+
+import Segmentator from "./segmentator";
 import React from "react";
 
 export default class Playtomaton extends React.Component {
   constructor(props) {
     super(props);
-    // FIXME
-    this.ws = this.props.wavesurfer;
     this.handlePlayPause = this.handlePlayPause.bind(this);
     this.settings = {
       repeatRegion: 3
@@ -13,13 +17,51 @@ export default class Playtomaton extends React.Component {
       loopRegion: null,
       loopCount: 0
     };
+  }
 
-    wavesurfer.on("region-click", (region, event) => {
+  render() {
+    return (
+      <div className="playtomaton">
+        <button onClick={this.handlePlayPause}>play</button>
+        <div ref={(node) => { this.wsNode = node; }}></div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.ws = WaveSurfer.create({
+      container: this.wsNode,
+      height: 60,
+      barWidth: 2,
+      hideScrollbar: true,
+      interact: false
+    });
+    if (this.props.src) {
+      this.ws.load(this.props.src);
+    }
+
+    this.ws.on("ready", () => {
+      let minimap = this.ws.initMinimap({
+        height: 30,
+        barHeight: 10,
+        barWidth: null,
+        interact: true
+      });
+      this.ws.zoom(25);
+      let peaks = this.ws.backend.getPeaks(100000, 0, 99999);
+      let segmentator = new Segmentator();
+      let segments = segmentator.findSegments(peaks, this.ws.getDuration());
+      for(let i in segments) {
+        this.ws.addRegion(segments[i]);
+      }
+    });
+
+    this.ws.on("region-click", (region, event) => {
       this.clearLoopRegion();
       this.seekToRegion(region);
     });
 
-    wavesurfer.on("region-in", (region, event) => {
+    this.ws.on("region-in", (region, event) => {
       if (this.state.loopRegion !== region) {
         this.setState({
           loopRegion: region,
@@ -28,7 +70,7 @@ export default class Playtomaton extends React.Component {
       }
     });
 
-    wavesurfer.on("region-out", (region, event) => {
+    this.ws.on("region-out", (region, event) => {
       if (this.state.loopRegion === region) {
         let newLoopCount = this.state.loopCount + 1;
         this.setState({loopCount: newLoopCount});
@@ -41,21 +83,13 @@ export default class Playtomaton extends React.Component {
     });
   }
 
-  render() {
-    return (
-      <div className="playtomaton">
-        <button onClick={this.handlePlayPause}>play</button>
-      </div>
-    );
-  }
-
   handlePlayPause() {
     this.ws.playPause();
   }
 
   seekToRegion(region) {
     let progress = region.start / this.ws.getDuration();
-    wavesurfer.seekTo(progress);
+    this.ws.seekTo(progress);
   }
 
   clearLoopRegion() {
